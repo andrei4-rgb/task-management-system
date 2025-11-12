f2a73892a00e2ad68b1232cea8dff67b0cf7a1cb
ganda <barboaxlekyle@gmail.com>
2025-11-12 12:35:00 +0800

    added new ready to merge

---
+++ docker-php83/laravel/app/Http/Controllers/TaskControllers.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::orderBy('created_at','desc')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'nullable|in:pending,in-progress,completed',
+            'due_date' => 'nullable|date',
+        ]);
+        return Task::create($data);
+    }
+
+    public function show($id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return $task;
+    }
+
+    public function destroy($id)
+    {
+        Task::destroy($id);
+        return response()->json(null, 204);
+    }
+}
